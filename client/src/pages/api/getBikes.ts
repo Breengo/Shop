f9ca1735c179fa317ba35fd6c3c69bdb4d6c19c4
Greengo <vipser988@gmail.com>
2023-05-06 17:25:18 +0700

    check problems with api

--- client/src/pages/api/getBikes.ts
+++ client/src/pages/api/getBikes.ts
@@ -22,5 +22,32 @@ export default function handler(
 ) {
   const filter = req.query;
 
-  res.status(200).json(bikes);
+  res.status(200).json(
+    bikes.filter((item) => {
+      let optionMatch = 0;
+      for (let key in filter) {
+        let newKey = key.split(" ");
+        newKey[0] = newKey[0].toLowerCase();
+        if (newKey[1]) {
+          newKey[1] = newKey[1][0].toUpperCase() + newKey[1].slice(1);
+        }
+        const option = filter[key];
+        key = newKey.join("");
+        if (item[key as keyof typeof item] === option) {
+          optionMatch++;
+        }
+        if (key === "milleage" && item["milleage"] === 0) {
+          optionMatch++;
+        }
+      }
+      if (
+        item.price < Number(filter["upperPrice"]) &&
+        item.price > Number(filter["bottomPrice"])
+      ) {
+        optionMatch += 2;
+      }
+      if (optionMatch === Object.keys(filter).length) return true;
+      else return false;
+    })
+  );
 }
