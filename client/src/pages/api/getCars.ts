cd3162347c3e0b78104497d0996a25ad0518ef87
Greengo <vipser988@gmail.com>
2023-05-10 16:38:15 +0700

    add error handler for api

--- client/src/pages/api/getCars.ts
+++ client/src/pages/api/getCars.ts
@@ -21,36 +21,41 @@ export default function handler(
 ) {
   const filter = req.query;
 
-  if (filter) {
-    return res.status(200).json(
-      cars.filter((item) => {
-        let optionMatch = 0;
-        for (let key in filter) {
-          let newKey = key.split(" ");
-          newKey[0] = newKey[0].toLowerCase();
-          if (newKey[1]) {
-            newKey[1] = newKey[1][0].toUpperCase() + newKey[1].slice(1);
+  try {
+    if (filter) {
+      return res.status(200).json(
+        cars.filter((item) => {
+          let optionMatch = 0;
+          for (let key in filter) {
+            let newKey = key.split(" ");
+            newKey[0] = newKey[0].toLowerCase();
+            if (newKey[1]) {
+              newKey[1] = newKey[1][0].toUpperCase() + newKey[1].slice(1);
+            }
+            const option = filter[key];
+            key = newKey.join("");
+            if (item[key as keyof typeof item] === option) {
+              optionMatch++;
+            }
+            if (key === "milleage" && item["milleage"] === 0) {
+              optionMatch++;
+            }
           }
-          const option = filter[key];
-          key = newKey.join("");
-          if (item[key as keyof typeof item] === option) {
-            optionMatch++;
+          if (
+            item.price < Number(filter["upperPrice"]) &&
+            item.price > Number(filter["bottomPrice"])
+          ) {
+            optionMatch += 2;
           }
-          if (key === "milleage" && item["milleage"] === 0) {
-            optionMatch++;
-          }
-        }
-        if (
-          item.price < Number(filter["upperPrice"]) &&
-          item.price > Number(filter["bottomPrice"])
-        ) {
-          optionMatch += 2;
-        }
-        if (optionMatch === Object.keys(filter).length) return true;
-        else return false;
-      })
-    );
-  } else {
-    return res.status(200).json(cars);
+          if (optionMatch === Object.keys(filter).length) return true;
+          else return false;
+        })
+      );
+    } else {
+      return res.status(200).json(cars);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
   }
 }
